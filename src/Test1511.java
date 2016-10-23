d0d072050148b6c7bba30774231a2db6db1820f7
hachizero <helpme@i-um.net>
2016-10-22 20:33:07 -0700

    2.0

--- src/Test1511.java
+++ src/Test1511.java
@@ -5,5 +5,6 @@ public class Test1511 {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Heelo World!!");
+        System.out.println("2.0");
     }
 }
--- src/test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by a on 2016. 10. 22..
- */
-public class test {
-}
